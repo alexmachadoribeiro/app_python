e101579a899ecbc61f9477fc1d7279a4c886186c
alexmachadoribeiro <alex.ribeiro@df.docente.senai.br>
2025-05-21 19:46:14 -0300

    Commit inicial da aula do dia 21/05/2025

--- atividade_automacao_01/main.py
+++ atividade_automacao_01/main.py
@@ -1,3 +1,33 @@
+import pyautogui as p
+
+def limpar_credenciais():
+    p.write("git config --global --unset user.name")
+    p.press("enter")
+    p.write("git config --global --unset user.email")
+    p.press("enter")
+
+if __name__ == "__main__":
+    p.PAUSE = 0.5
+
+    msg_commit = input("Informe a mensagem do commit: ")
+
+    # p.hotkey("ctrl", "j")
+
+    limpar_credenciais()
+
+    p.write('git config --global user.name "alexmachadoribeiro"')
+    p.press("enter")
+    p.write('git config --global user.email "alex.ribeiro@df.docente.senai.br"')
+    p.press("enter")
+    p.write("git add .")
+    p.press("enter")
+    p.write(f'git commit -m "{msg_commit}"')
+    p.press("enter")
+    p.write("git push")
+    p.press("enter")
+
+    limpar_credenciais()
+
 # TODO - atividade
 """
 Crie um programa que commita o projeto de vocês e envia o repositório para o GitHub.
